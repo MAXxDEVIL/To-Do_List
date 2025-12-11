407704f493be52b2186fa4e4d3b09f2df75d1b89
MAXxDEVIL <paramitagopalpaul06@gmail.com>
2025-12-11 15:39:10 +0530

    adding js

--- console.js
+++ console.js
@@ -1,7 +1,28 @@
-const inputBox = document.getElementById("input-box");
+const inputBox = document.getElementById("inputBox");
 const listcotainer = document.getElementById("listcotainer");
 
+function AddTask(){
+    if(inputBox.value === ''){
+        alert("You must write something !");
+    }
 
-Addtask(){
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listcotainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
     
+    inputBox.value = "";
 }
+
+listcotainer.addEventListener("click",function(e){
+    if(e.terget.tagName === "li"){
+        e.target.classlist.toggle("checked");
+    }
+    else if(e.terget.tagName === "span"){
+        e.target.parentElement.remove(); 
+    }
+}, false);
